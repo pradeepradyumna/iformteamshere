e5ff3c2ef2850634b068e12b8c50f7b09d324c7e
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2020-12-24 13:03:25 +0530

    Applied random background color to Extras component

--- form-teams-here/src/components/Extras.js
+++ form-teams-here/src/components/Extras.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Extras({ name, items }) {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
   return (
     <div className="card-column">
       {/* <div className="card"> */}
-      <div className="notebook-editable-extras">
+      <div
+        className="notebook-editable-extras"
+        style={{ backgroundColor: "#" + `${randomColor}` }}
+      >
         <h4 className="notebook-editable-p">{name}</h4>
         <ul>
           {items.map((item) => (
