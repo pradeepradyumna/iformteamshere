c1662788e392a0dd44161bfb0a481c49f74983b7
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2020-12-28 15:21:48 +0530

    Added styling to heart present in footer

--- form-teams-here/src/components/Footer.js
+++ form-teams-here/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <p>Copyright &copy; {new Date().getFullYear()} All Rights Reserved</p>
       </div>
       <div className="my-name">
-        Made with &#10084;&#65039; by &nbsp;
+        Made with <span className="lds-heart">&#10084;&#65039;</span> by &nbsp;
         <a
           href="https://gistcdn.githack.com/pradeepradyumna/073fe0b8033edaa6f5fd8364940d697f/raw/86c3a1391fbc32281d0160efd93932df2c47c8a4/index.html"
           target="_blank"
